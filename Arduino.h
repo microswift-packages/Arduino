642de3a642e8ae22715194bff43fca3b9daebbf9
Carl Peto <carl.peto@me.com>
2023-08-16 09:51:22 +0100

    add interrupts from libc

--- Arduino.h
+++ Arduino.h
@@ -23,6 +23,8 @@
 // #include "Client.h"
 // #include "Server.h"
 
+#include <avr/interrupts.h>
+
 #define min(a,b) ((a)<(b)?(a):(b))
 #define max(a,b) ((a)>(b)?(a):(b))
 // #define abs(x) ((x)>0?(x):-(x))
